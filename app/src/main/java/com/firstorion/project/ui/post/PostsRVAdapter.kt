a89b4fbd40884470ea20128fc8f7aa2d5412834c
MichaelJahns <jahnsmic001@gmail.com>
2021-05-01 08:45:25 -0700

    Roughs in the RV Adapter

--- app/src/main/java/com/firstorion/project/ui/post/PostsRVAdapter.kt
+++ app/src/main/java/com/firstorion/project/ui/post/PostsRVAdapter.kt
@@ -1,31 +1,41 @@
 package com.firstorion.project.ui.post
 
+import android.print.PrintDocumentAdapter
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.firstorion.project.R
 import com.firstorion.project.repo.post.Post
 
 /**
  * Feel free to change as you wish
  * */
 class PostsRVAdapter(
-    private val postClickedListener: OnPostClickedListener
+    private val postClickedListener: OnPostClickedListener,
+    private val postList: List<Post>
 ): RecyclerView.Adapter<PostsRVAdapter.PostViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.post_row, parent, false)
+        return PostViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.postTitle.text = postList[position].title
+        holder.postBody.text = postList[position].body
+        holder.postUserId.text = postList[position].userId.toString()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return postList.size
     }
 
     inner class PostViewHolder(v: View): RecyclerView.ViewHolder(v){
-
+        val postTitle: TextView = v.findViewById(R.id.tvPostTitle)
+        val postBody: TextView = v.findViewById(R.id.tvPostBody)
+        val postUserId: TextView = v.findViewById(R.id.tvPostUserId)
     }
 
     interface OnPostClickedListener{
