0c49d1caa5bb1e28e4a2f50935f408f3f381e6f7
MichaelJahns <jahnsmic001@gmail.com>
2021-05-05 08:39:59 -0700

    Deletes unused files, rearranges Repos to more closer resemble each other

--- app/src/main/java/com/firstorion/project/viewmodel/post/PostsViewModel.kt
+++ app/src/main/java/com/firstorion/project/viewmodel/post/PostsViewModel.kt
@@ -1,15 +1,10 @@
 package com.firstorion.project.viewmodel.post
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
 import com.firstorion.project.repo.post.Post
 
 import com.firstorion.project.repo.post.PostRepository
-import com.firstorion.project.util.Resource
-import kotlinx.coroutines.Dispatchers
-import java.lang.Exception
 
 /**
  * This class will get the `Post` data from `postRepo` that will be displayed on `PostsFragment` fragment.
